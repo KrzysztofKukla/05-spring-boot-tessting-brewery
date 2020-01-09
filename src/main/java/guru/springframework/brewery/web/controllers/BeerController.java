ed19f7edce4d6f96eb7d5e7df267d12ca322abd9
Krzysztof Kukla <krzys.kukla@gmail.com>
2020-01-10 00:02:56 +0100

    182 jsonPath with Captor on list

--- src/main/java/guru/springframework/brewery/web/controllers/BeerController.java
+++ src/main/java/guru/springframework/brewery/web/controllers/BeerController.java
@@ -24,7 +24,11 @@ import guru.springframework.brewery.web.model.BeerStyleEnum;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
 
@@ -32,8 +36,8 @@ import java.util.UUID;
 @RestController
 public class BeerController {
 
-    private static final Integer DEFAULT_PAGE_NUMBER = 0;
-    private static final Integer DEFAULT_PAGE_SIZE = 25;
+    static final Integer DEFAULT_PAGE_NUMBER = 0;
+    static final Integer DEFAULT_PAGE_SIZE = 25;
 
     private final BeerService beerService;
 
@@ -41,7 +45,7 @@ public class BeerController {
         this.beerService = beerService;
     }
 
-    @GetMapping(produces = { "application/json" })
+    @GetMapping(produces = {"application/json"})
     public ResponseEntity<BeerPagedList> listBeers(@RequestParam(value = "pageNumber", required = false) Integer pageNumber,
                                                    @RequestParam(value = "pageSize", required = false) Integer pageSize,
                                                    @RequestParam(value = "beerName", required = false) String beerName,
--- src/test/java/guru/springframework/brewery/web/controllers/BeerControllerTest.java
+++ src/test/java/guru/springframework/brewery/web/controllers/BeerControllerTest.java
@@ -2,16 +2,24 @@ package guru.springframework.brewery.web.controllers;
 
 import guru.springframework.brewery.services.BeerService;
 import guru.springframework.brewery.web.model.BeerDto;
+import guru.springframework.brewery.web.model.BeerPagedList;
 import guru.springframework.brewery.web.model.BeerStyleEnum;
+import org.assertj.core.util.Lists;
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.ArgumentMatchers;
 import org.mockito.BDDMockito;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -19,11 +27,11 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.math.BigDecimal;
-import java.math.BigInteger;
 import java.time.OffsetDateTime;
+import java.util.List;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -41,11 +49,11 @@ class BeerControllerTest {
 
     private MockMvc mockMvc;
 
-    private BeerDto beerDto;
+    private BeerDto beerDto1;
 
     @BeforeEach
     void setUp() {
-        beerDto = BeerDto.builder()
+        beerDto1 = BeerDto.builder()
             .id(UUID.randomUUID())
             .beerName("first beer")
             .beerStyle(BeerStyleEnum.LAGER)
@@ -65,13 +73,77 @@ class BeerControllerTest {
 
     @Test
     void getBeerById() throws Exception {
-        BDDMockito.when(beerService.findBeerById(ArgumentMatchers.any())).thenReturn(beerDto);
+        BDDMockito.when(beerService.findBeerById(ArgumentMatchers.any())).thenReturn(beerDto1);
 
-        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/beer/{beerId}",UUID.randomUUID()))
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/beer/{beerId}", UUID.randomUUID()))
             .andExpect(status().isOk())
             .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON_UTF8))
-            .andExpect(jsonPath("$.id", Matchers.is(beerDto.getId().toString())))
-            .andExpect(MockMvcResultMatchers.jsonPath("$.beerName",Matchers.is(beerDto.getBeerName())));
+            .andExpect(jsonPath("$.id", Matchers.is(beerDto1.getId().toString())))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.beerName", Matchers.is(beerDto1.getBeerName())));
+    }
+
+    @DisplayName(value = "List operations-> ")
+    @Nested
+    class TestListOperation {
+
+        @Captor
+        private ArgumentCaptor<String> beerNameCaptor;
+
+        @Captor
+        private ArgumentCaptor<BeerStyleEnum> beerStyleCaptor;
+
+        @Captor
+        private ArgumentCaptor<PageRequest> pageRequestCaptor;
+
+        BeerPagedList beerPagedList;
+
+        BeerDto beerDto2;
+
+        @BeforeEach
+        void setUp() {
+            beerDto2 = BeerDto.builder()
+                .id(UUID.randomUUID())
+                .version(2)
+                .beerName("second beer")
+                .beerStyle(BeerStyleEnum.LAGER)
+                .price(new BigDecimal(23))
+                .upc(10L)
+                .createdDate(OffsetDateTime.now())
+                .lastModifiedDate(OffsetDateTime.now())
+                .quantityOnHand(10)
+                .build();
+
+            List<BeerDto> beerDtoList = Lists.list(beerDto1, beerDto2);
+
+            BeerPagedList beerPagedList = new BeerPagedList(beerDtoList, PageRequest.of(1, 1), 2L);
+
+            BDDMockito.given(beerService.listBeers(beerNameCaptor.capture(), beerStyleCaptor.capture(),
+                pageRequestCaptor.capture())).willReturn(beerPagedList);
+
+        }
+
+        @DisplayName(value = "Test list beers without any parameters-> ")
+        @Test
+        void noParameters() throws Exception {
+            mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/beer")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content", Matchers.hasSize(2)))
+                //to be aware this works only for specific order like here
+                //if we are getting order from database there is no guarantees such orders
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content[0].beerName", Matchers.is(beerDto1.getBeerName())))
+                .andExpect(jsonPath("$.content[1].version", Matchers.is(beerDto2.getVersion())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content[1].beerStyle", Matchers.is(beerDto2.getBeerStyle().name())));
+
+            PageRequest pageRequestCaptured = pageRequestCaptor.getValue();
+            Assertions.assertNotNull(pageRequestCaptured);
+            Assertions.assertEquals(BeerController.DEFAULT_PAGE_NUMBER.intValue(), pageRequestCaptured.getPageNumber());
+            Assertions.assertEquals(BeerController.DEFAULT_PAGE_SIZE.intValue(), pageRequestCaptured.getPageSize());
+            Assertions.assertNull(beerNameCaptor.getValue());
+            Assertions.assertNull(beerStyleCaptor.getValue());
+        }
+
     }
 
 }
